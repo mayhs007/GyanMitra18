0dede7a217fa68949c92627bfcb07028595c64bf
mayhs007 <31859454+mayhs007@users.noreply.github.com>
2018-01-20 16:31:15 +0530

    finalia

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesSeeder::class);    
         $this->call(UsersSeeder::class);       
         $this->call(ConfigsSeeder::class);
+        $this->call(DepartmentsSeeder::class);
     }
 }
---
+++ database/seeds/DepartmentsSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DepartmentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('departments')->insert([
+            ['name' => 'CIVIL'],
+            ['name' => 'CSE/IT'],
+            ['name' => 'ECE'],
+            ['name' => 'EEE'],
+            ['name' => 'MECH'],
+            ['name' => 'BIO-TECH'],
+            ['name' => 'MCA'],
+            ['name' => 'MATHS']
+            
+            
+                    ]);
+    }
+}
